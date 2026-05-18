266926c0370079d3e17fa2044d05da2f5265e047
malekdalpirou765-hue <malekdalpirou765@gmail.com>
2026-05-18 15:14:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 async function translateText() {
 
-  const text = document.getElementById("text").value;
+const text =
+document.getElementById("text").value;
 
-  const response = await fetch(
-    "https://openrouter.ai/api/v1/chat/completions",
-    {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer YOUR_API_KEY",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        model: "openai/gpt-4o-mini",
-        messages: [
-          {
-            role: "user",
-            content:
-            `Translate professionally to English: ${text}`
-          }
-        ]
-      })
-    }
-  );
+const response = await fetch(
+"https://openrouter.ai/api/v1/chat/completions",
+{
+method: "POST",
 
-  const data = await response.json();
+headers: {
+"Authorization":
+"Bearer YOUR_API_KEY",
 
-  document.getElementById("result").innerText =
-    data.choices[0].message.content;
+"Content-Type":
+"application/json"
+},
+
+body: JSON.stringify({
+
+model: "openai/gpt-4o-mini",
+
+messages: [
+{
+role: "user",
+
+content:
+`Translate this to English:
+${text}`
+}
+]
+})
+}
+);
+
+const data = await response.json();
+
+document.getElementById("result").innerText =
+data.choices[0].message.content;
+
 }
